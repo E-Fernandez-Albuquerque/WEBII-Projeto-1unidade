5baa35fa59927851f8829b72129413004c33e45b
Talles <71418062+Lalset@users.noreply.github.com>
2021-12-02 20:54:51 -0300

    Adicionando delete

--- concessionaria/carros/views.py
+++ concessionaria/carros/views.py
@@ -5,6 +5,7 @@ from django.http import request
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 class CarroListView(ListView):
     model = Carro
@@ -39,3 +40,12 @@ def editarVeiculo(request, id):
 
     else:
         return render(request, 'atualizar.html',{'form': form, 'edit':edit})
+
+@login_required
+def deletarVeiculo(request, id):
+    carro = get_object_or_404(Carro, pk=id)
+    carro.delete()
+
+    messages.info(request, 'Carro deletado com sucesso.')
+
+    return redirect('/concessionaria')
